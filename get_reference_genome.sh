0a45c38480e81d6d2efe1020b3ee6256bb188719
Maria Fernandes <79261854+FernandesMaria@users.noreply.github.com>
2023-03-08 11:35:17 +0000

    get ref script update

--- get_reference_genome.sh
+++ get_reference_genome.sh
@@ -10,8 +10,10 @@ mkdir ref_genome
 wget -P ref_genome/ https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/109.20211119/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz
 
 ## 3- Uncompress reference genome and delete compressed version (to keep the compressed file too add -k option )
-gzip -d GCF_000001405.39_GRCh38.p13_genomic.fna.gz
+gzip -d ref_genome/GCF_000001405.39_GRCh38.p13_genomic.fna.gz
 
 ## 4- Split reference per chromosome
-python ref_per_chr.python
-
+python ref_per_chr.py GCF_000001405.39_GRCh38.p13_genomic.fna _GRCh38.p13_genomic.fna
+## Excludes 'scaffold', 'patch', 'mitochondrion' sequences 
+## If others to exclude add condition to line 24 
+## outputname format: chr<num>_<suffix_user_given> (line above: chr<num>_GRCh38.p13_genomic.fna)
