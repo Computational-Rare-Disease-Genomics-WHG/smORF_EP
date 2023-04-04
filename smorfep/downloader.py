c99152bbf29b0a0240b6b2b4d8ee2df68f7b706f
FernandesMaria <79261854+FernandesMaria@users.noreply.github.com>
2023-04-04 17:23:09 +0100

    update download ref

--- smorfep/downloader.py
+++ smorfep/downloader.py
@@ -26,17 +26,21 @@ def download_ref_genome(ref_link):
     os.makedirs('ref_genome', exist_ok=True)
 
     # 2. Download reference from NCBI to the ref_genome/repository -- For GRC38.p13
-    url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/109.20211119/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz'
-    urllib.request.urlretrieve(url, 'ref_genome/GCF_000001405.39_GRCh38.p13_genomic.fna.gz')
+    ##url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/109.20211119/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz'
+    
+    ## get the filename from path (last bit): 
+    outputname = ref_link.split('/')[-1] 
+    outputname_unconpress = outputname.strip('.gz')
+    urllib.request.urlretrieve(ref_link, 'ref_genome/' + outputname)
 
     # 3. Uncompress reference genome and delete compressed version
-    with gzip.open('ref_genome/GCF_000001405.39_GRCh38.p13_genomic.fna.gz', 'rb') as f_in:
-        with open('ref_genome/GCF_000001405.39_GRCh38.p13_genomic.fna', 'wb') as f_out:
+    with gzip.open('ref_genome/' + outputname, 'rb') as f_in:
+        with open('ref_genome/'+ outputname_unconpress, 'wb') as f_out:
             f_out.write(f_in.read())
-    os.remove('ref_genome/GCF_000001405.39_GRCh38.p13_genomic.fna.gz')
+    os.remove('ref_genome/' + outputname)
 
     # 4. Split reference per chromosome
-    os.system('ref_per_chr.py GCF_000001405.39_GRCh38.p13_genomic.fna _GRCh38.p13_genomic.fna')
+    os.system('ref_per_chr.py ref_genome/' + outputname_unconpress + ' _GRCh38.p13_genomic.fna')
     # Excludes 'scaffold', 'patch', 'mitochondrion' sequences 
     # If others to exclude add condition to line 24 
     # outputname format: chr<num>_<suffix_user_given> (line above: chr<num>_GRCh38.p13_genomic.fna)
@@ -47,7 +51,7 @@ def download_ref_genome(ref_link):
 
 def download_gencode(transc_link):
     """
-    Downloads Gencode
+    Downloads Gencode gff3 format (extensions used are .gff3 specific)
 
     Input: 
     -transc_link: Full link for the transcripts gff3 file (we used GENCODE)
@@ -65,17 +69,21 @@ def download_gencode(transc_link):
     os.makedirs('transcripts', exist_ok=True)
 
     # 2- Download GENCODE
-    url = 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_41/gencode.v41.annotation.gff3.gz'
-    urllib.request.urlretrieve(url, 'transcripts/gencode.v41.annotation.gff3.gz')
+    ##url = 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_41/gencode.v41.annotation.gff3.gz'
+    outputname = transc_link.split('/')[-1] 
+    outputname_unconpress = outputname.strip('.gz')
+
+    urllib.request.urlretrieve(transc_link, 'transcripts/'+ outputname)
 
     # 3- Uncompress reference genome and delete compressed version
-    with gzip.open('transcripts/gencode.v41.annotation.gff3.gz', 'rb') as f_in:
-        with open('transcripts/gencode.v41.annotation.gff3', 'wb') as f_out:
+    with gzip.open('transcripts/' + outputname, 'rb') as f_in:
+        with open('transcripts/'+ outputname_unconpress, 'wb') as f_out:
             f_out.write(f_in.read())
-    os.remove('transcripts/gencode.v41.annotation.gff3.gz')
+    os.remove('transcripts/'+ outputname)
 
     # 4- Precomputations on genecode file
-    # Pre-process gff3 file - single header
+    # Pre-process gff3 file - single header ## TODO: make the names 
+    prefix = outputname_unconpress.strip('.gff3')
     os.system('preProcess_gff.py transcripts/gencode.v41.annotation.gff3 transcripts/gencode.v41.annotation_columnNames.gff3')
 
     # transcript coordinates (15.seconds M1 ship)
