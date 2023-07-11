28e753c71c99b0f6ce3de5373f8b04c7ecbbc143
Maria Fernandes <79261854+FernandesMaria@users.noreply.github.com>
2023-07-11 14:29:31 +0100

    Intron rev (#83)
    * Fix splice region --> splice acceptor
    
    * fixed del within splice region
    
    * Fixed frameshift --> frameshift,splice region
    
    * fix ref path
    
    * fix frameshift --> splice acceptor when 1 nt in the exon and 1 in the intron
    
    * fix indel size --> int
    
    * added unique variant ID

--- smorfep/generate_examples.py
+++ smorfep/generate_examples.py
@@ -36,7 +36,7 @@ def main():
     parser.add_argument('-i','--orfID', metavar='\b', required=True, type=str, help='ORF ID')
     parser.add_argument('-ws','--windowStart', metavar='\b', required=True, type=int, help='variants region start coordinate')
     parser.add_argument('-we','--windowEnd', metavar='\b', required=True, type=int, help='variants region end coordinate')
-    parser.add_argument('-m','--indelMaxSize', metavar='\b', required=True, type=str, help='indel maximum size')
+    parser.add_argument('-m','--indelMaxSize', metavar='\b', required=True, type=int, help='indel maximum size')
     parser.add_argument('-r','--refpath', metavar='\b', required=True, type=str, help='Path to the reference genome')
     parser.add_argument('-o', '--outputfile', metavar='\b', required=True, type=str, help='output file name')
     parser.add_argument('-e', '--exonNts', metavar='\b', type=int, default=3, help='output file name')
@@ -50,18 +50,21 @@ def main():
     all_nts = ['A', 'T', 'G', 'C']
 
     ## load the reeference genome chr sequence
-    files_prefix, files_suffix = check_prefix_sufix_ref_files(args.ref_path)
-    ref = read_single_fasta(args.chrom, args.ref_path, files_prefix, files_suffix)
+    files_prefix, files_suffix = check_prefix_sufix_ref_files(args.refpath)
+    ref = read_single_fasta(args.chrom, args.refpath, files_prefix, files_suffix)
 
     ## get sequence
     seq_left = get_sequence(args.windowStart-args.exonNts-1, args.windowStart+args.intronNts-1+args.indelMaxSize, '+', ref)
-    print(args.windowStart, args.exonNts, args.intronNts)
-    print(args.windowStart-args.exonNts-1, args.windowStart+args.intronNts-1)
-    print(seq_left)
+    # print(args.windowStart, args.exonNts, args.intronNts)
+    # print(args.windowStart-args.exonNts-1, args.windowStart+args.intronNts-1)
+    # print(seq_left)
 
     ## output open and header
     out = open(args.outputfile, 'w')
     out.write('chrm\tvar_pos\tref\talt\tstart\tend\tstrand\tvar_id\tsmorf_id')
+
+    var_index = 1
+    var_id = 'XXX'+str(var_index)
 
     ## NOTE: starts 1 postion before the exons nts -- as we use anchor notation
     for var_size in range(args.indelMaxSize+1): ## +1 to include the args.indelMaxSize
@@ -69,6 +72,8 @@ def main():
         diff = args.indelMaxSize-var_size
         iter_seq = seq_left[:len(seq_left)-diff]
         ##print(iter_seq)
+
+        var_id = 'XXX'+str(var_index)
 
         if var_size == 0: ## SNV -- runs only once
             for each_nt in range(len(iter_seq)-var_size):
@@ -81,7 +86,8 @@ def main():
 
                 ## uncoment next line for SNV
                 ##out.write('\n'+chrom+'\t'+str(var_pos)+'\t'+ref+'\t'+alt+'\t'+orf_start+'\t'+orf_end+'\t'+strand+'\t'+var_type+'\t'+orf_id)
-
+                ##var_index +=1
+                ##var_id = 'XXX'+str(var_index)
 
 
         else: ## insertion and deletion  
@@ -98,9 +104,13 @@ def main():
 
                 # print(var_pos, ref_ins, alt_ins, var_type_ins)
                 # print(var_pos, ref_del, alt_del, var_type_del)
-                out.write('\n'+args.chrom+'\t'+str(var_pos)+'\t'+ref_ins+'\t'+alt_ins+'\t'+args.orfStart+'\t'+args.orfEnd+'\t'+args.strand+'\t'+var_type_ins+'\t'+args.orfID)
-                out.write('\n'+args.chrom+'\t'+str(var_pos)+'\t'+ref_del+'\t'+alt_del+'\t'+args.orfStart+'\t'+args.orfEnd+'\t'+args.strand+'\t'+var_type_del+'\t'+args.orfID)
+                out.write('\n'+args.chrom+'\t'+str(var_pos)+'\t'+ref_ins+'\t'+alt_ins+'\t'+args.orfStart+'\t'+args.orfEnd+'\t'+args.strand+'\t'+var_id +'_'+var_type_ins+'\t'+args.orfID)
+                var_index +=1
+                var_id = 'XXX'+str(var_index)
+                out.write('\n'+args.chrom+'\t'+str(var_pos)+'\t'+ref_del+'\t'+alt_del+'\t'+args.orfStart+'\t'+args.orfEnd+'\t'+args.strand+'\t'+var_id +'_'+var_type_del+'\t'+args.orfID)
 
+                var_index +=1
+                var_id = 'XXX'+str(var_index)
 
     out.close()
 
