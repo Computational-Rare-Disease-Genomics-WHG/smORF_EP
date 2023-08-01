d03338eef62f1261a831545e895d03cd0a8b3058
FernandesMaria <79261854+FernandesMaria@users.noreply.github.com>
2023-08-01 02:24:31 +0100

    insertion between acceptor site nts -- splice_acceptor_variant: OK

--- smorfep/utils/functions.py
+++ smorfep/utils/functions.py
@@ -2070,7 +2070,12 @@ def check_exon_intron_vars(seq, start_orf, end_orf, var_pos, ref, alt, strand, m
             
 
         elif var_type == 'insertion': 
-            if var_start_check == True and var_end_check == True and [x for x in check_no_anchor if x in splice_region] != []: ## variant within the exon, but on the splice region -- last 3 nt of the exon (VEP default)
+
+            if len([x for x in all_var_pos if x in donor_acceptor_positions]) == len(donor_acceptor_positions) and var_pos in donor_acceptor_positions: ## insertion in the middle of the acceptor main site
+                dna_cons = 'splice_acceptor_variant'
+                prot_cons = '-'
+
+            elif var_start_check == True and var_end_check == True and [x for x in check_no_anchor if x in splice_region] != []: ## variant within the exon, but on the splice region -- last 3 nt of the exon (VEP default)
                 insertion_size = len(alt) -1 ## -1 to remove anchor base
 
                 if insertion_size % 3 == 0:
