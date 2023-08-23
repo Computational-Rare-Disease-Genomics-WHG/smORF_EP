4dd5285068e4c4148a16da28abee2a5eca01323d
Maria Fernandes <79261854+FernandesMaria@users.noreply.github.com>
2023-08-23 15:24:53 +0100

    Forwardstrand fix (#130)
    * computations of insertion frame
    
    * removed protein_altering_variant from inframe insertion block
    
    * rm protein_altering annotation
    
    * run_testing script updated

--- smorfep/test/run_testing.sh
+++ smorfep/test/run_testing.sh
@@ -2,62 +2,30 @@
 
 date=$(date '+%Y-%m-%d')
 
-## root to the dir
-cd /Users/mariaf/Desktop/GitHub/smORF_EP/smorfep/test
+echo $date
+
+## CNBD1 donor [no missing variants testing file used]
+##smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f CNBD1_1-13nts_donor.vcf -o CNBD1_1-13nts_donor_consequences_${date}.tsv > testing_donor${date}.log
+
+##compare2vep -s CNBD1_1-13nts_donor_consequences_${date}.tsv -v VEP_analysis_CNBD1_donor_1-13nts.vcf -o compare_CNBD1_donor_1-13nts_VEP_${date}.tsv
+
+## CNBD1 acceptor [XX TODO]
+##smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f CNBD1_1-13nts_acceptor.vcf -o CNBD1_1-13nts_acceptor_consequences_${date}.tsv > testing_acceptor${date}.log
+
+##compare2vep -s CNBD1_1-13nts_acceptor_consequences_${date}.tsv -v VEP_analysis_CNBD1_acceptor_1-13nts.vcf -o compare_CNBD1_acceptor_1-13nts_VEP_${date}.tsv
 
 
-##
-## run smorfep for donor and acceptor, forward and reverse strand -- testing examples
-smorfep \
--r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ \
--t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv \
--i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv \
--f SMAD2_1-3nts_acceptor.vcf \
--o SMAD2_1-3nts_acceptor_consequences_${date}.vcf
+## SMAD2 donor [TODO] missing 44
+smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f SMAD2_1-13nts_donor.vcf -o SMAD2_1-13nts_donor_consequences_${date}.tsv > testing_SMAD2_donor${date}.log
 
-smorfep \
--r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ \
--t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv \
--i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv \
--f SMAD2_1-3nts_donor.vcf \
--o SMAD2_1-3nts_donor_consequences_${date}.vcf
-
-## forward strand
-smorfep \
--r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ \
--t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv \
--i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv \
--f CNBD1_1-3nts_acceptor.vcf \
--o CNBD1_1-3nts_acceptor_consequences_${date}.vcf
-
-smorfep \
--r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ \
--t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv \
--i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv \
--f CNBD1_1-3nts_donor.vcf \
--o CNBD1_1-3nts_donor_consequences_${date}.vcf
+compare2vep -s SMAD2_1-13nts_donor_consequences_${date}.tsv -v VEP_analysis_SMAD2_donor_1-13nts.vcf -o compare_SMAD2_donor_1-13nts_VEP_${date}.tsv
 
 
-## 
-## Compare the results with VEP annotations
-compare2vep \
--s SMAD2_1-3nts_donor_consequences_${date}.vcf \
--v VEP_analysis_SMAD2_donor.vcf \
--o compare_smorf_VEP_SMAD2_1-3nts_donor_${date}.vcf
-
-compare2vep \
--s SMAD2_1-3nts_acceptor_consequences_${date}.vcf \
--v SMAD2_1-3nts_acceptor.vcf \
--o compare_smorf_VEP_SMAD2_1-3nts_acceptor_${date}.vcf
 
 
-## forward strand
-compare2vep \
--s CNBD1_1-3nts_donor_consequences_${date}.vcf \
--v VEP_analysis_CNBD1_donor.vcf \
--o compare_smorf_VEP_CNBD1_1-3nts_donor_${date}.vcf
+## SMAD2 acceptor [TODO] missing 40
+##smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f SMAD2_1-13nts_acceptor.vcf -o SMAD2_1-13nts_acceptor_consequences_${date}.tsv > testing_SMAD2_acceptor${date}.log
 
-compare2vep \
--s CNBD1_1-3nts_acceptor_consequences_${date}.vcf \
--v VEP_analysis_CNBD1_acceptor.vcf \
--o compare_smorf_VEP_CNBD1_1-3nts_acceptor_${date}.vcf
+##compare2vep -s SMAD2_1-13nts_acceptor_consequences_${date}.tsv -v VEP_analysis_SMAD2_acceptor_1-13nts.vcf -o compare_SMAD2_acceptor_1-13nts_VEP_${date}.tsv
+
+
--- smorfep/utils/functions.py
+++ smorfep/utils/functions.py
@@ -1852,13 +1852,14 @@ def check_introns(seq, start_orf, end_orf, var_pos, ref, alt, strand, map_gen2tr
                 ##print(seq_aa, seq_change_aa)
 
                 if insertion_size % 3 == 0:
-                    if seq_change_aa == '': ## change to a stop codon
-                        dna_cons = 'inframe_insertion&splice_region_variant'
-                    elif seq_aa != seq_change_aa: 
-                        dna_cons = 'protein_altering_variant&splice_region_variant'
-                    else: 
-                        dna_cons = 'inframe_insertion&splice_region_variant'
+                    # if seq_change_aa == '': ## change to a stop codon
+                    #     dna_cons = 'inframe_insertion&splice_region_variant'
+                    # elif seq_aa != seq_change_aa: 
+                    #     dna_cons = 'protein_altering_variant&splice_region_variant'
+                    # else: 
+                    #     dna_cons = 'inframe_insertion&splice_region_variant'
 
+                    dna_cons = 'inframe_insertion&splice_region_variant'
                     prot_cons = '-'
                 else: 
                     dna_cons = 'frameshift_variant&splice_region_variant' ## frameshift_insertion
@@ -1868,20 +1869,17 @@ def check_introns(seq, start_orf, end_orf, var_pos, ref, alt, strand, map_gen2tr
                 print('var_pos check True, var_next_positio_check False, check_no_anchor within splice region exon')
 
                 if insertion_size % 3 == 0:
-                    if insertion_size > 6 : ## longer insertion for some reason where considered inframe insertion rather than proteing altering 
-                        
-                        ## Check if there is a stop inframe for all the insertion length -- not limited to the first codon as a stop
-                        seq_new = seq[:map_gen2transc[splice_region_exon_nts[-1]]+1] ## from begining until the intron
-                        seq_insertion = seq_new + alt[1:] ## adds all the alternative without the anchor nt
-                        find_stop = find_stop_inframe_sequence(seq_insertion)
-                        
-                        if find_stop == True: ## if the insertion introduces a stop codon
-                            dna_cons = 'stop_gained&protein_altering_variant&splice_region_variant'
-                        else: 
-                            dna_cons = 'inframe_insertion&splice_region_variant'
 
-                    else:
-                        dna_cons = 'protein_altering_variant&splice_region_variant'
+                    ## Check if there is a stop inframe for all the insertion length -- not limited to the first codon as a stop
+                    seq_new = seq[:map_gen2transc[splice_region_exon_nts[-1]]+1] ## from begining until the intron
+                    seq_insertion = seq_new + alt[1:] ## adds all the alternative without the anchor nt
+                    find_stop = find_stop_inframe_sequence(seq_insertion)
+                    
+                    if find_stop == True: ## if the insertion introduces a stop codon
+                        dna_cons = 'stop_gained&splice_region_variant'
+                    else: 
+                        dna_cons = 'inframe_insertion&splice_region_variant'
+
                 else: 
                     dna_cons = 'frameshift_variant&splice_region_variant' ## frameshift_insertion
                 prot_cons = '-'
@@ -2092,20 +2090,43 @@ def check_introns(seq, start_orf, end_orf, var_pos, ref, alt, strand, map_gen2tr
 
             elif var_start_check == True and var_end_check == True and [x for x in check_no_anchor if x in splice_region_exon_nts] != [] and strand == '+': ## variant within the exon, but on the splice region -- last 3 nt of the exon (VEP default)
                 print('first protein altering condition')
-                seq_new = seq[map_gen2transc[splice_region_exon_nts[0]]: map_gen2transc[splice_region_exon_nts[-1]]+1]
-                seq_aa = get_protein(seq_new) 
-                seq_changed = seq[map_gen2transc[splice_region_exon_nts[0]]:map_gen2transc[var_pos]+1] + alt[1:] + seq[map_gen2transc[var_pos]+1: map_gen2transc[splice_region_exon_nts[-1]]+1]
-                seq_change_aa = get_protein(seq_changed[:3])  ### collects just the first codon
-                print('seq_aa initial', seq_aa)
-                print('seq_change aa', seq_change_aa)
+                # seq_new = seq[map_gen2transc[splice_region_exon_nts[0]]: map_gen2transc[splice_region_exon_nts[-1]]+1]
+                # seq_aa = get_protein(seq_new) 
+                # seq_changed = seq[map_gen2transc[splice_region_exon_nts[0]]:map_gen2transc[var_pos]+1] + alt[1:] + seq[map_gen2transc[var_pos]+1: map_gen2transc[splice_region_exon_nts[-1]]+1]
+                # seq_change_aa = get_protein(seq_changed[:3])  ### collects just the first codon
+                # print('seq_aa initial', seq_aa)
+                # print('seq_change aa', seq_change_aa)
+
+                print(seq)
+
+                nt_aa_mapping = nt2aaMAP(seq)
+                ##print(nt_aa_mapping)
+                ## as the var_pos is intronic, we search for var_next_pos
+                var_pos_index = map_gen2transc[var_pos] +1 ## +1 as counting in the genome starts at 1 and python 0
+                print(var_pos_index)
+                check_insertion_frame = var_pos_index%3 ## if = 0 --> inframe
+                ##check_insertion_frame
+                print('frame of next position', check_insertion_frame)
+                if check_insertion_frame == 0: 
+                    exon_codon = seq[var_pos_index:var_pos_index+3]
+                    new_codon = 0 ## TODO : add variant and compute the changes
+
+                elif check_insertion_frame == 1: 
+                    exon_codon = seq[var_pos_index-1:var_pos_index+2]
+                    new_codon = 0 ## TODO : add variant and compute the changes
+
+                elif check_insertion_frame == 2: 
+                    exon_codon = seq[var_pos_index-2:var_pos_index+1]
+                    new_codon = 0 ## TODO : add variant and compute the changes
+                print('exon codon', exon_codon)
+
+
+
+
 
                 if insertion_size % 3 == 0:
-                    check_insertion_frame = var_pos+1-splice_region_exon_nts[0] ## +1 so the anchor is removed
                     print(check_insertion_frame)
-                    if check_insertion_frame % 3 == 0: 
-                        dna_cons = 'protein_altering_variant&splice_region_variant'
-                    else: 
-                        dna_cons = 'inframe_insertion&splice_region_variant'
+                    dna_cons = 'inframe_insertion&splice_region_variant'
                 else: 
                     dna_cons = 'frameshift_variant&splice_region_variant' ## frameshift_insertion
                 prot_cons = '-'
@@ -2143,19 +2164,20 @@ def check_introns(seq, start_orf, end_orf, var_pos, ref, alt, strand, map_gen2tr
                 ##print('end false, start true, - strand')
 
                 ## we need to invert as the indexes are strand based
-                exon_codon = seq[map_gen2transc[splice_region_exon_nts[-1]]:map_gen2transc[splice_region_exon_nts[0]]+1]
-                seq_aa = get_protein(exon_codon)
+                # exon_codon = seq[map_gen2transc[splice_region_exon_nts[-1]]:map_gen2transc[splice_region_exon_nts[0]]+1]
+                # seq_aa = get_protein(exon_codon)
 
-                ##if var_pos not in splice_region_exon_nts: ## insertion between the last nt of the intron and the first of the exon
-                changed_seq = alt[1:] + exon_codon
-                changed_codon = changed_seq[:3]
-                changed_seq_aa = get_protein(changed_codon)
+                # ##if var_pos not in splice_region_exon_nts: ## insertion between the last nt of the intron and the first of the exon
+                # changed_seq = alt[1:] + exon_codon
+                # changed_codon = changed_seq[:3]
+                # changed_seq_aa = get_protein(changed_codon)
 
                 if insertion_size % 3 == 0:
-                    if seq_aa != changed_seq_aa:
-                        dna_cons = 'protein_altering_variant&splice_region_variant'
-                    else: 
-                        dna_cons = 'inframe_insertion&splice_region_variant'
+                    dna_cons = 'inframe_insertion&splice_region_variant'
+                    # if seq_aa != changed_seq_aa:
+                    #     dna_cons = 'protein_altering_variant&splice_region_variant'
+                    # else: 
+                    #     dna_cons = 'inframe_insertion&splice_region_variant'
                 else: 
                     dna_cons = 'frameshift_variant&splice_region_variant' ## frameshift_insertion
                 prot_cons = '-'
@@ -2180,42 +2202,51 @@ def check_introns(seq, start_orf, end_orf, var_pos, ref, alt, strand, map_gen2tr
                 print('insertion on the last intron base and chack protein change, + strand')
                 
                 if insertion_size % 3 == 0:
-                    print(seq)
+                    dna_cons = 'inframe_insertion&splice_region_variant'
+                    # print(seq)
 
-                    nt_aa_mapping = nt2aaMAP(seq)
-                    ## as the var_pos is intronic, we search for var_next_pos
-                    next_var_pos_index = map_gen2transc[var_next_pos] +1 ## +1 as counting in the genome starts at 1 and python 0
-                    print(next_var_pos_index)
-                    print(seq[next_var_pos_index-1:next_var_pos_index+3])
-                    check_insertion_frame = next_var_pos_index % 3 ## if = 0 --> inframe
-                    ##check_insertion_frame
-                    print('frame of next position', next_var_pos_index % 3 )
-
-
-
-                    exon_codon = seq[map_gen2transc[splice_region_exon_nts[0]]:map_gen2transc[splice_region_exon_nts[-1]]+1]
-                    seq_aa = get_protein(exon_codon)
-
-                    ##if var_pos not in splice_region_exon_nts: ## insertion between the last nt of the intron and the first of the exon
-                    changed_seq = alt[1:] + exon_codon
-                    changed_codon = changed_seq[:3]
-                    changed_seq_aa = get_protein(changed_codon)
-
-                    print(seq_aa, changed_seq_aa)
-                    
+                    # nt_aa_mapping = nt2aaMAP(seq)
+                    # ##print(nt_aa_mapping)
+                    # ## as the var_pos is intronic, we search for var_next_pos
+                    # next_var_pos_index = map_gen2transc[var_next_pos] +1 ## +1 as counting in the genome starts at 1 and python 0
+                    # print(next_var_pos_index)
+                    # print(seq[next_var_pos_index:next_var_pos_index+3])
+                    # check_insertion_frame = next_var_pos_index%3 ## if = 0 --> inframe
+                    # ##check_insertion_frame
+                    # print('frame of next position', check_insertion_frame)
+                    # if check_insertion_frame == 0: 
+                    #     exon_codon = seq[next_var_pos_index-1:next_var_pos_index-1+3]
+                    # elif check_insertion_frame == 1: 
+                    #     exon_codon = seq[next_var_pos_index-1-1:next_var_pos_index-1+2]
+                    # elif check_insertion_frame == 2: 
+                    #     exon_codon = seq[next_var_pos_index-1-2:next_var_pos_index-1+1]
+                    # print('exon codon', exon_codon)
 
 
-                    if check_insertion_frame == 0: 
-                        if insertion_size > 3: 
-                            print('inframe and >3')
-                            dna_cons = 'protein_altering_variant&splice_region_variant'
-                        else:   
-                            dna_cons = 'inframe_insertion&splice_region_variant'
+
+                    # exon_codon = seq[map_gen2transc[splice_region_exon_nts[0]]:map_gen2transc[splice_region_exon_nts[-1]]+1]
+                    # seq_aa = get_protein(exon_codon)
+
+                    # ##if var_pos not in splice_region_exon_nts: ## insertion between the last nt of the intron and the first of the exon
+                    # changed_seq = alt[1:] + exon_codon
+                    # changed_codon = changed_seq[:3]
+                    # changed_seq_aa = get_protein(changed_codon)
+
+                    # print(seq_aa, changed_seq_aa)
+        
+
+                    # if check_insertion_frame == 0: 
+                    #     if insertion_size > 3: 
+                    #         print('inframe and >3')
+                    #         dna_cons = 'protein_altering_variant&splice_region_variant'
+                    #     else:   
+                    #         ## inframe condition
+                    #         dna_cons = 'inframe_insertion&splice_region_variant'
                 
-                        
-                    else:
-                        print('other condition protein altering')
-                        dna_cons = 'protein_altering_variant&splice_region_variant'
+                    # else:
+                    #     print('other condition protein altering')
+                    #     dna_cons = 'protein_altering_variant&splice_region_variant'
+
 
                 else: 
                     dna_cons = 'frameshift_variant&splice_region_variant' ## frameshift_insertion
