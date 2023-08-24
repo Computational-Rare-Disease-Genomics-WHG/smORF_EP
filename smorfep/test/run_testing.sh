4eef26c46616e64fbb37404ee2bda9663caad5a7
Maria Fernandes <79261854+FernandesMaria@users.noreply.github.com>
2023-08-24 18:02:09 +0100

    run testing scriot (#136)

--- smorfep/test/run_testing.sh
+++ smorfep/test/run_testing.sh
@@ -24,16 +24,17 @@ echo " "
 ## reverse strand
 ## SMAD2 donor
 echo "- strand donor"
-smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f SMAD2_1-13nts_acceptor.vcf -o SMAD2_1-13nts_acceptor_consequences_${date}.tsv > testing_SMAD2_acceptor${date}.log
-
-compare2vep -s SMAD2_1-13nts_acceptor_consequences_${date}.tsv -v VEP_analysis_SMAD2_acceptor_1-13nts.vcf -o compare_SMAD2_acceptor_1-13nts_VEP_${date}.tsv
-echo " "
-
-## SMAD2 acceptor
-echo "- strand acceptor"
 smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f SMAD2_1-13nts_donor.vcf -o SMAD2_1-13nts_donor_consequences_${date}.tsv > testing_SMAD2_donor${date}.log
 
 compare2vep -s SMAD2_1-13nts_donor_consequences_${date}.tsv -v VEP_analysis_SMAD2_donor_1-13nts.vcf -o compare_SMAD2_donor_1-13nts_VEP_${date}.tsv
+echo " "
+
+
+## SMAD2 acceptor
+echo "- strand acceptor"
+smorfep -r /Users/mariaf/Desktop/GitHub/smORF_EP/ref_genome/ -t /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_transcriptCoord_2023-04-25.tsv  -i /Users/mariaf/Desktop/GitHub/smORF_EP/transcripts/gencode.v41.annotation_introns_2023-04-25.tsv -f SMAD2_1-13nts_acceptor.vcf -o SMAD2_1-13nts_acceptor_consequences_${date}.tsv > testing_SMAD2_acceptor${date}.log
+
+compare2vep -s SMAD2_1-13nts_acceptor_consequences_${date}.tsv -v VEP_analysis_SMAD2_acceptor_1-13nts.vcf -o compare_SMAD2_acceptor_1-13nts_VEP_${date}.tsv
 echo " "
 
 echo "DONE" 
