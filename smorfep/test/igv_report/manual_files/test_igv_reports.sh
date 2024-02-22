5bf1880be30ce46744acc43c3ff069e9ccd99816
Maria Fernandes <79261854+FernandesMaria@users.noreply.github.com>
2024-02-22 02:32:37 +0000

    Igv reports module  (#142)
    * script to generate an html igv report
    
    * function to generate igv-report
    
    * generate_ivg_files - variants.vcf:misisng clinvar annotations
    
    * generate_ivg_files - main_smorf.bed:ongoing
    
    * generate_ivg_files - main_smorf.bed:done
    
    * comment on flanking size igv-reports
    
    * generate_ivg_files - overlap.bed:ongoing
    
    * generate_ivg_files - overlap.bed:ongoing-start and end conditions
    
    * generate_ivg_files - overlap.bed:ongoing-filtering overlaping smorfs; missing deduplication
    
    * Dez 2023 changes commit
    
    * script to generate the report

--- smorfep/test/igv_report/manual_files/test_igv_reports.sh
+++ smorfep/test/igv_report/manual_files/test_igv_reports.sh
@@ -2,11 +2,14 @@
 
 ## script to generate the report per smORF -- variants we want to look up 
 
+date_today=$(date '+%Y-%m-%d')
+##echo ${date_today}
+
 create_report variants.vcf \ ##SMAD2_noheader.bed \
---fasta ../../../ref_genome/hg38.fa \
+--fasta ../../../../ref_genome/hg38.fa \
 --flanking 1000 \
 --info-columns PRED_EFFECT SMORF CLNSIG CLNREVSTAT \
---tracks SMAD2_noheader.bed variants.vcf overlap.bed \
---output test_example_genome_2.html
+--tracks SMAD2_noheader.bed variants_pathogenic.vcf variants_uncertain.vcf variants_benign.vcf overlap.bed \
+--output test_example_genome_${date_today}.html
 
 ##--fasta https://igv-genepattern-org.s3.amazonaws.com/genomes/seq/hg38/hg38.fa \
